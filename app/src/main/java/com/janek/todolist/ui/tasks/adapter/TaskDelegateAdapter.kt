e48253329274d91e597c2bc83c039f67dd7d5a55
Janek Brandt <brandt.janek@gmail.com>
2018-01-23 20:08:09 -0800

    refactor task list item to remove unnecessary textView

--- app/src/main/java/com/janek/todolist/ui/tasks/adapter/TaskDelegateAdapter.kt
+++ app/src/main/java/com/janek/todolist/ui/tasks/adapter/TaskDelegateAdapter.kt
@@ -11,7 +11,6 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.TextView
 import com.janek.todolist.R
 import com.janek.todolist.commons.adapter.ViewType
 import com.janek.todolist.commons.adapter.ViewTypeDelegateAdapter
@@ -40,7 +39,6 @@ class TaskDelegateAdapter(
     ) : RecyclerView.ViewHolder(itemView) {
 
         private val checkBox: CheckBox = itemView.findViewById(R.id.task_checkbox)
-        private val taskText: TextView = itemView.findViewById(R.id.task_text)
         private val taskEdit: EditText = itemView.findViewById(R.id.task_edit_input)
         private val taskDelete: ImageButton = itemView.findViewById(R.id.task_delete)
         private var taskItem: TaskItem? = null
@@ -48,20 +46,18 @@ class TaskDelegateAdapter(
         init {
             checkBox.setOnCheckedChangeListener { _, checked ->
                 onTaskComplete(taskItem!!, checked)
-                taskText.paintFlags = taskText.paintFlags xor Paint.STRIKE_THRU_TEXT_FLAG
+                taskEdit.paintFlags = taskEdit.paintFlags xor Paint.STRIKE_THRU_TEXT_FLAG
             }
-
-            taskText.setOnClickListener { startEdit() }
 
             taskDelete.setOnClickListener { onTaskDelete(taskItem!!) }
 
-            taskEdit.imeOptions = EditorInfo.IME_ACTION_DONE
-            taskEdit.setRawInputType(InputType.TYPE_CLASS_TEXT)
+//            taskEdit.imeOptions = EditorInfo.IME_ACTION_DONE
+            taskEdit.setRawInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS)
 
             taskEdit.setOnEditorActionListener { _, action, _ ->
                 when(action) {
                     EditorInfo.IME_ACTION_DONE -> {
-                        endEdit()
+                        updateTask()
                         true
                     }
                     else -> false
@@ -70,40 +66,38 @@ class TaskDelegateAdapter(
 
             taskEdit.setOnFocusChangeListener { _, hasFocus ->
                 if (!hasFocus) {
-                    endEdit()
+                    updateTask()
                 }
             }
+
+            // on keyboard close submit edit
 
         }
 
         fun bind(task: TaskItem) {
             taskItem = task
-            taskText.text = task.text
+            taskEdit.setText(task.text)
             checkBox.isChecked = task.done
 
             if (task.text.isEmpty()) {
-                startEdit()
+                showKeyboard()
             }
         }
 
-        private fun startEdit() {
-            taskText.visibility = View.INVISIBLE
-            taskEdit.visibility = View.VISIBLE
+        private fun updateTask() {
+            val updatedText = taskEdit.text.toString()
+            onTaskEdit(taskItem!!, updatedText)
+            hideKeyboard()
+        }
 
-            taskEdit.setText(taskItem?.text)
+        private fun showKeyboard() {
             taskEdit.requestFocus()
-            taskEdit.setSelection(taskItem!!.text.length)
             (taskEdit.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
                     .toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
         }
 
-        private fun endEdit() {
-            val updatedText = taskEdit.text.toString()
-            taskText.text = updatedText
-            taskText.visibility = View.VISIBLE
-            taskEdit.visibility = View.INVISIBLE
-
-            onTaskEdit(taskItem!!, updatedText)
+        private fun hideKeyboard() {
+            taskEdit.clearFocus()
             (taskEdit.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
                     .hideSoftInputFromWindow(taskEdit.windowToken, 0)
         }
