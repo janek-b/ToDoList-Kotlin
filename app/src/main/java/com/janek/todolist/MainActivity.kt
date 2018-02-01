caf4fde57d76e21c2b3670270a0a0b998d4bb2fa
Janek Brandt <brandt.janek@gmail.com>
2018-02-01 15:26:05 -0800

    initial list of task lists implementation
    very basic implementation of list of task lists allowing to create new lists and open individual lists

--- app/src/main/java/com/janek/todolist/MainActivity.kt
+++ app/src/main/java/com/janek/todolist/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.janek.todolist
 
+import android.arch.persistence.room.Room
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.janek.todolist.data.db.AppDatabase
+import com.janek.todolist.data.models.TaskList
+import com.janek.todolist.ui.tasks.TasksActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val taskListDao = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "todo-list").allowMainThreadQueries().build().taskListDao()
+
+        val taskListAdapter = TaskListAdapter(
+                { taskList -> startActivity(TasksActivity.createIntent(this, taskList.id)) }
+        )
+
+        task_list_recyclerview.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = taskListAdapter
+        }
+
+        taskListDao.getAllTaskLists()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { taskListAdapter.setItems(it) }
+
+        new_list_button.setOnClickListener {
+            val newListName = new_list_edit_text.text.toString()
+            new_list_edit_text.text.clear()
+            taskListDao.insert(TaskList(newListName))
+        }
     }
 }
---
+++ app/src/main/java/com/janek/todolist/TaskListAdapter.kt
@@ -0,0 +1,50 @@
+package com.janek.todolist
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.janek.todolist.commons.extensions.inflate
+import com.janek.todolist.data.models.TaskList
+
+class TaskListAdapter(private val openList: (TaskList) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private var items: List<TaskList>
+
+    init {
+        items = emptyList()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return TaskListViewHolder(parent.inflate(R.layout.task_list), openList)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        (holder as TaskListViewHolder).bind(items[position])
+    }
+
+    fun setItems(newItems: List<TaskList>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+    class TaskListViewHolder(itemView: View,
+                             private val openList: (TaskList) -> Unit) : RecyclerView.ViewHolder(itemView) {
+        private val listName: TextView = itemView.findViewById(R.id.task_list_name)
+        private var taskList: TaskList? = null
+
+        init {
+            listName.setOnClickListener {
+                openList(taskList!!)
+            }
+        }
+
+        fun bind(list: TaskList) {
+            listName.text = list.name
+            taskList = list
+        }
+
+    }
+}
