fa2ff10f3a3be2eca78e4db81c81cb9412be2743
Janek Brandt <brandt.janek@gmail.com>
2018-02-01 14:30:14 -0800

    add TaskListDao to room database

--- app/src/main/java/com/janek/todolist/data/db/AppDatabase.kt
+++ app/src/main/java/com/janek/todolist/data/db/AppDatabase.kt
@@ -7,4 +7,5 @@ import com.janek.todolist.data.models.TaskItem
 @Database(entities = arrayOf(TaskItem::class), version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskItemDao(): TaskItemDao
+    abstract fun taskListDao(): TaskListDao
 }
